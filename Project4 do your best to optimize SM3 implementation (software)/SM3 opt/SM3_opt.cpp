d553fb81c00e9c95af82c95930eab264763da1c7
snipernan <93069394+snipernan@users.noreply.github.com>
2023-07-26 13:03:59 +0800

    Update SM3_opt.cpp

--- Project4 do your best to optimize SM3 implementation (software)/SM3 opt/SM3_opt.cpp
+++ Project4 do your best to optimize SM3 implementation (software)/SM3 opt/SM3_opt.cpp
@@ -6,8 +6,17 @@
 #include <sstream>
 #include<stdio.h>
 #include <openssl/evp.h>
+#include <immintrin.h>
 
 #define SM3_DIGEST_LENGTH 32
+#define FF0(x, y, z) (_mm256_xor_si256((x), _mm256_xor_si256((y), (z))))
+#define FF1(x, y, z) (_mm256_or_si256(_mm256_and_si256(y, z), _mm256_or_si256(_mm256_and_si256(x, y), _mm256_and_si256(x, z))))//(X & Y | X & Z | Y & Z)
+#define GG0(x, y, z) (_mm256_xor_si256((x), _mm256_xor_si256((y), (z))))
+#define GG1(x, y, z) (_mm256_or_si256(_mm256_and_si256(x, y), _mm256_and_si256(_mm256_andnot_si256(x, _mm256_set1_epi32(-1)), z)))//(X & Y | ~X & Z)
+#define P00(x) (_mm256_xor_si256(_mm256_xor_si256((x), ROTL(x, 9)),ROTL(x, 17)))
+#define P10(x) (_mm256_xor_si256(_mm256_xor_si256((x), ROTL(x, 15)),ROTL(x, 23)))
+
+#define ROTL(x, n) (_mm256_or_si256(_mm256_slli_epi32((x), (n)), _mm256_srli_epi32((x), 32 - (n))))
 using namespace std;
 
 static const uint32_t IV[8] = { 0x7380166f, 0x4914b2b9, 0x172442d7, 0xda8a0600, 0xa96f30bc, 0x163138aa, 0xe38dee4d, 0xb0fb0e4e };
@@ -48,7 +57,7 @@ uint8_t* padding(uint8_t* m, int l) {
 	memcpy(padded_m, m, l);
 	memset(padded_m + l, 0, new_l - l - 8);
 	padded_m[l] = 0x80;
-	uint64_t bit_len = (uint64_t)l * 8;	
+	uint64_t bit_len = (uint64_t)l * 8;
 	uint8_t buf[8];
 	for (int i = 0; i < 8; i++) {
 		buf[i] = (bit_len >> ((7 - i) * 8)) & 0xff;
@@ -64,14 +73,106 @@ uint32_t le32toh(uint32_t x) {
 		((x & 0xff000000) >> 24);
 }
 
+
+__m256i rotl(__m256i v, int n) {
+	__m256i mask = _mm256_set_epi32(0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, (1 << n) - 1);
+	__m256i rotated = _mm256_slli_epi32(v, n);
+	rotated = _mm256_and_si256(rotated, mask);
+	return rotated;
+}
+
+void sm3_compress_avx2(const uint8_t* m, uint32_t V[8]) {
+	int n = strlen((char*)m) / 64;
+	for (int j = 0; j <= n; j++) {
+		__m256i A = _mm256_set1_epi32(V[0]);
+		__m256i B = _mm256_set1_epi32(V[1]);
+		__m256i C = _mm256_set1_epi32(V[2]);
+		__m256i D = _mm256_set1_epi32(V[3]);
+		__m256i E = _mm256_set1_epi32(V[4]);
+		__m256i F = _mm256_set1_epi32(V[5]);
+		__m256i G = _mm256_set1_epi32(V[6]);
+		__m256i H = _mm256_set1_epi32(V[7]);
+
+		__m256i W[68];
+		for (int i = 0; i < 16; i++) {
+			__m256i w = _mm256_loadu_si256((__m256i*)(m + j *64 + i * 4 * sizeof(uint8_t)));
+			W[i] = _mm256_shuffle_epi8(w, _mm256_set_epi32(0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00010203));
+		}
+
+		for (int i = 16; i < 68; i++) {
+			__m256i w1 = W[i - 16];
+			__m256i w2 = W[i - 9];
+			__m256i w3 = ROTL(W[i - 3], 15);
+			__m256i w4 = _mm256_xor_si256(w1, w2);
+			__m256i w5 = _mm256_xor_si256(w4, w3);
+			__m256i w6 = P10(w5);
+			__m256i w7 = _mm256_xor_si256(w6, ROTL(W[i - 13], 7));
+			W[i] = _mm256_xor_si256(w7, W[i - 6]);
+
+		}
+
+		__m256i W0[64];
+		for (int i = 0; i < 64; i++) {
+			W0[i] = _mm256_xor_si256(W[i], W[i + 4]);
+		}
+
+		for (int i = 0; i < 16; i++) {
+			__m256i SS1_0 = _mm256_add_epi32(ROTL(A, 12), E);
+			__m256i SS1_1 = _mm256_add_epi32(SS1_0, ROTL(_mm256_set1_epi32(T(i)), i));
+			__m256i SS1 = ROTL(SS1_1, 7);
+
+			__m256i SS2 = _mm256_xor_si256(SS1, ROTL(A, 12));
+
+			__m256i TT1 = _mm256_add_epi32(_mm256_add_epi32(_mm256_add_epi32(FF0(A, B, C), D), SS2), W0[i]);
+			__m256i TT2 = _mm256_add_epi32(_mm256_add_epi32(_mm256_add_epi32(GG0(E, F, G), H), SS1), W[i]);
+			D = C;
+			C = _mm256_or_si256(ROTL(B, 9), _mm256_srli_epi32(B, 23));
+			B = A;
+			A = TT1;
+			H = G;
+			G = _mm256_or_si256(ROTL(F, 19), _mm256_srli_epi32(F, 13));
+			F = E;
+			E = P00(TT2);
+		}
+		for (int i = 16; i < 64; i++) {
+			__m256i SS1_0 = _mm256_add_epi32(ROTL(A, 12), E);
+			__m256i SS1_1 = _mm256_add_epi32(SS1_0, ROTL(_mm256_set1_epi32(T(i)), i%32));
+			__m256i SS1 = ROTL(SS1_1, 7);
+
+			__m256i SS2 = _mm256_xor_si256(SS1, ROTL(A, 12));
+			__m256i TT1 = _mm256_add_epi32(_mm256_add_epi32(_mm256_add_epi32(FF1(A, B, C), D), SS2), W0[i]);
+			__m256i TT2 = _mm256_add_epi32(_mm256_add_epi32(_mm256_add_epi32(GG1(E, F, G), H), SS1), W[i]);
+			D = C;
+			C = _mm256_or_si256(ROTL(B, 9), _mm256_srli_epi32(B, 23));
+			B = A;
+			A = TT1;
+			H = G;
+			G = _mm256_or_si256(ROTL(F, 19), _mm256_srli_epi32(F, 13));
+			F = E;
+			E = P00(TT2);
+		}
+
+
+		V[0] ^= _mm256_extract_epi32(A, 0) ;
+		V[1] ^= _mm256_extract_epi32(B, 0) ;
+		V[2] ^= _mm256_extract_epi32(C, 0) ;
+		V[3] ^= _mm256_extract_epi32(D, 0) ;
+		V[4] ^= _mm256_extract_epi32(E, 0) ;
+		V[5] ^= _mm256_extract_epi32(F, 0) ;
+		V[6] ^= _mm256_extract_epi32(G, 0) ;
+		V[7] ^= _mm256_extract_epi32(H, 0) ;
+	}
+}
+
+
 void iterate(uint8_t* m, uint32_t* V) {
 	int n = strlen((char*)m) / 64;
 	uint32_t B[16];
-	    for (int i = 0; i <=n; i++) {
-        for (int j = 0; j < 16; j++) {
-            // 将每个 32 位字的字节序进行反转
-            B[j] = le32toh(((uint32_t*)m)[i * 16 + j]);
-        }
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j < 16; j++) {
+			// 将每个 32 位字的字节序进行反转
+			B[j] = le32toh(((uint32_t*)m)[i * 16 + j]);
+		}
 		uint32_t A = V[0], B1 = V[1], C = V[2], D = V[3], E = V[4], F = V[5], G = V[6], H = V[7];
 		uint32_t W[68];
 		for (int j = 0; j < 16; j++) {
@@ -116,7 +217,8 @@ void openssl_sm3(const unsigned char* msg, size_t msglen, unsigned char dgst[SM3
 	EVP_Digest(msg, msglen, dgst, &out_size, EVP_sm3(), nullptr);
 }
 
-void testSM3() {
+
+int main() {
 	// 输入消息
 	const int MESSAGE_SIZE = 64; //单位是字节
 	int message[MESSAGE_SIZE] = { 0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,0x61,0x62,0x63,0x64,
@@ -136,7 +238,9 @@ void testSM3() {
 	uint32_t out_size = 32;
 	// 计时
 	auto start = chrono::high_resolution_clock::now();
-	EVP_Digest(msg1, 64, dgst, &out_size, EVP_sm3(), nullptr);
+	for (int i = 0; i < 1000; i++) {
+		EVP_Digest(msg1, 64, dgst, &out_size, EVP_sm3(), nullptr);
+	}
 	auto end = chrono::high_resolution_clock::now();
 
 	printf("SM3 哈希值为：");
@@ -155,11 +259,13 @@ void testSM3() {
 	cout << endl;
 	uint8_t pre_hash[32];
 	start = chrono::high_resolution_clock::now();
-	EVP_MD_CTX* mdctx;
-	mdctx = EVP_MD_CTX_new();
-	EVP_DigestInit_ex(mdctx, EVP_sm3(), NULL);
-	EVP_DigestUpdate(mdctx, msg1, 64);
-	EVP_DigestFinal_ex(mdctx, pre_hash, NULL);
+	for (int i = 0; i < 1000; i++) {
+		EVP_MD_CTX* mdctx;
+		mdctx = EVP_MD_CTX_new();
+		EVP_DigestInit_ex(mdctx, EVP_sm3(), NULL);
+		EVP_DigestUpdate(mdctx, msg1, 64);
+		EVP_DigestFinal_ex(mdctx, pre_hash, NULL);
+	}
 	end = chrono::high_resolution_clock::now();
 	// 输出 SM3 哈希值
 	cout << "Hash(input): ";
@@ -175,9 +281,12 @@ void testSM3() {
 	// Measure time
 
 	uint32_t result[8];
-	memcpy(result, IV, sizeof(IV));
+
 	start = chrono::high_resolution_clock::now();
-	sm3(msg1, sizeof(msg1), result);
+	for (int i = 0; i < 1000; i++) {
+		memcpy(result, IV, sizeof(IV));
+		sm3(msg1, sizeof(msg1), result);
+	}
 	end = chrono::high_resolution_clock::now();
 	cout << endl << "Hash(input): ";
 	for (int i = 0; i < 8; i++) {
@@ -187,10 +296,6 @@ void testSM3() {
 
 	// 输出时间
 	duration = chrono::duration_cast<chrono::microseconds>(end - start);
-	cout << "优化版SM3计算时间为：" << dec<< duration.count() << " 微秒" << endl;
-}
-
-int main() {
-	testSM3();
+	cout << "优化版SM3计算时间为：" << dec << duration.count() << " 微秒" << endl;
 }
 
