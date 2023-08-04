8f92b65337b0b649ff647e22403c85c2b21c37b4
snipernan <93069394+snipernan@users.noreply.github.com>
2023-08-04 13:37:03 +0800

    Update Schnorr Bacth.cpp

--- Project21 Schnorr Bacth/Schnorr Bacth/Schnorr Bacth.cpp
+++ Project21 Schnorr Bacth/Schnorr Bacth/Schnorr Bacth.cpp
@@ -7,9 +7,18 @@
 #include <openssl/ec.h>
 #include <openssl/ecdsa.h>
 #include <openssl/obj_mac.h>
+#include <windows.h>
 
 
 EC_GROUP* curve = EC_GROUP_new_by_curve_name(NID_secp256k1);
+
+int jacobi(const BIGNUM* a, const BIGNUM* n, BN_CTX* ctx) {
+    int ret = BN_kronecker(a, n, ctx);
+    if (ret == -2) {
+        return 0;
+    }
+    return ret;
+}
 
 // 输出EC_POINT对象的坐标
 void printECPointCoordinates(const EC_POINT* point) {
@@ -32,6 +41,7 @@ void printECPointCoordinates(const EC_POINT* point) {
     BN_free(x);
     BN_free(y);
 }
+
 
 std::vector<unsigned char> bytes_point(const EC_POINT* p) {
     const EC_GROUP* curve = EC_GROUP_new_by_curve_name(NID_secp256k1);
@@ -72,6 +82,7 @@ std::vector<unsigned char> schnorr_sign(const std::vector<unsigned char>& msg, c
 
     // 将公钥转换为字节数组
     std::vector<unsigned char> pk_bytes(33);
+    //pk_bytes = bytes_point(pubkey);
     EC_POINT_point2oct(curve, pubkey, POINT_CONVERSION_COMPRESSED, pk_bytes.data(), pk_bytes.size(), ctx);
 
     // 将私钥转换为字节数组
@@ -142,6 +153,113 @@ std::vector<unsigned char> schnorr_sign(const std::vector<unsigned char>& msg, c
     return sig_bytes;
 }
 
+bool schnorr_verify(const std::vector<unsigned char>& msg, EC_POINT* pubkey, const std::vector<unsigned char>& sig) {
+    // 确认公钥在椭圆曲线上
+    if (EC_POINT_is_on_curve(curve, pubkey, NULL) != 1) {
+        std::cerr << "Error: point is not on curve" << std::endl;
+        return false;
+    }
+
+    // 解析签名
+    BIGNUM* r = BN_bin2bn(sig.data(), 32, NULL);
+    BIGNUM* s = BN_bin2bn(sig.data() + 32, 32, NULL);
+
+
+    // 检查签名值是否在合法范围内
+    const BIGNUM* order = EC_GROUP_get0_order(curve);
+    if (BN_cmp(r, BN_value_one()) < 0 || BN_cmp(r, order) >= 0 || BN_cmp(s, BN_value_one()) < 0 || BN_cmp(s, order) >= 0) {
+        BN_free(r);
+        BN_free(s);
+        return false;
+    }
+
+    // 计算哈希值e
+    std::vector<unsigned char> e_data(32 + 34 + msg.size());
+    std::vector<unsigned char> pk_bytes(33);
+    EC_POINT_point2oct(curve, pubkey, POINT_CONVERSION_COMPRESSED, pk_bytes.data(), pk_bytes.size(), NULL);
+
+    std::copy(sig.data(), sig.data() + 32, e_data.begin());
+    std::copy(pk_bytes.begin(), pk_bytes.end(), e_data.begin() + 32);
+    std::copy(msg.begin(), msg.end(), e_data.begin() + 32 + 34);
+
+    unsigned char e[SHA256_DIGEST_LENGTH];
+
+    SHA256(e_data.data(), e_data.size(), e);
+    /*std::cout << "e_data: ";
+    for (const auto& byte : e_data) {
+        printf("%02x", byte);
+    }
+    printf("\n");
+    std::cout << "e: ";
+    for (const auto& byte : e) {
+        printf("%02x", byte);
+    }
+    printf("\n");*/
+
+    // 计算 R = s*G + (n - e)*pubkey
+    BIGNUM* n = BN_new();
+    EC_GROUP_get_order(curve, n, NULL);
+    BIGNUM* e_bn = BN_new();
+    BIGNUM* n_e = BN_new();
+    BN_bin2bn(e, sizeof(e), e_bn);
+    BN_sub(n_e, n, e_bn);
+    EC_POINT* R = EC_POINT_new(curve);
+    EC_POINT* G = EC_POINT_new(curve);
+    EC_POINT* G_s = EC_POINT_new(curve);
+    const EC_POINT* generator = EC_GROUP_get0_generator(curve);
+    EC_POINT_copy(G, generator);
+    EC_POINT* p_n_e = EC_POINT_new(curve);
+    EC_POINT_mul(curve, G_s, s, G, NULL, NULL);
+    //printECPointCoordinates(G_s);
+    //// 将 bn 输出到标准输出流
+    //hex_str = BN_bn2hex(n_e);
+    //if (hex_str != nullptr) {
+    //    printf("n-e = %s\n", hex_str);
+    //    OPENSSL_free(hex_str);
+    //}
+    //printECPointCoordinates(pubkey);
+    EC_POINT_mul(curve, p_n_e, n_e, pubkey, NULL, NULL);
+    //printECPointCoordinates(p_n_e);
+    EC_POINT_add(curve, R, G_s, p_n_e, NULL);
+
+    //printECPointCoordinates(R);
+
+    // 验证 R 是否为无穷远点
+    if (EC_POINT_is_at_infinity(curve, R)) {
+        return 0;
+    }
+
+    // 验证 R[0] 是否等于 r
+    BIGNUM* R_x = BN_new();
+    BIGNUM* R_y = BN_new();
+
+    BN_CTX* ctx = BN_CTX_new();
+    EC_POINT_get_affine_coordinates(curve, R, R_x, R_y, NULL);
+
+    if (R_x == NULL || R_y == NULL) {
+        return false;
+    }
+
+    BIGNUM* jacobiResult = BN_new();
+    jacobi(R_y, BN_value_one(),ctx);
+
+    if (BN_cmp(R_x, r) != 0 || BN_cmp(jacobiResult, BN_value_one()) != 0) {
+        BN_free(jacobiResult);
+        return false;
+    }
+
+    BN_free(jacobiResult);
+    return true;
+
+    // 释放资源
+    BN_free(r);
+    BN_free(s);
+    EC_POINT_free(G);
+    EC_POINT_free(R);
+
+    return 1;
+}
+
 bool schnorr_batch_verify(const std::vector<EC_POINT*>& pubkeys, const std::vector<std::vector<unsigned char>>& ms, const std::vector<std::vector<unsigned char>>& sigs) {
     EC_POINT* Rs = EC_POINT_new(curve);
     EC_POINT* Ps = EC_POINT_new(curve);
@@ -177,6 +295,16 @@ bool schnorr_batch_verify(const std::vector<EC_POINT*>& pubkeys, const std::vect
         unsigned char e[SHA256_DIGEST_LENGTH];
 
         SHA256(e_data.data(), e_data.size(), e);
+        //std::cout << "e_data: ";
+        //for (const auto& byte : e_data) {
+        //    printf("%02x", byte);
+        //}
+        //printf("\n");
+        //std::cout << "e: ";
+        //for (const auto& byte : e) {
+        //    printf("%02x", byte);
+        //}
+        //printf("\n");
 
 
         // 计算 c 和 y
@@ -230,17 +358,30 @@ bool schnorr_batch_verify(const std::vector<EC_POINT*>& pubkeys, const std::vect
         }
 
         // 更新 S、Rs 和 Ps
+
+        //char* hex_str = BN_bn2hex(S);
+        //if (hex_str != nullptr) {
+        //    printf("S = %s\n", hex_str);
+        //    OPENSSL_free(hex_str);
+        //}
         BIGNUM* s_a = BN_new();
         BN_mul(s_a, s, a, ctx);
         BN_add(S, S, s_a);
+        //hex_str = BN_bn2hex(S);
+        //if (hex_str != nullptr) {
+        //    printf("S = %s\n", hex_str);
+        //    OPENSSL_free(hex_str);
+        //}
 
-
+        //printECPointCoordinates(Rs);
         EC_POINT* Ra = EC_POINT_new(curve);
         EC_POINT* tmp3 = EC_POINT_new(curve);
         EC_POINT_set_affine_coordinates(curve, tmp3, r, y, nullptr);
         EC_POINT_mul(curve, Ra, NULL, tmp3, a, ctx);
         EC_POINT_add(curve, Rs, Rs, Ra, ctx);
+        //printECPointCoordinates(Rs);
 
+        //printECPointCoordinates(Ps);
         BIGNUM* e_bn = BN_new();
         BN_bin2bn(e, sizeof(e), e_bn);
         BIGNUM* a_times_e = BN_new();
@@ -249,6 +390,7 @@ bool schnorr_batch_verify(const std::vector<EC_POINT*>& pubkeys, const std::vect
         BN_mul(a_times_e, a, e_bn, ctx);
         EC_POINT_mul(curve, pubkey_a_e, NULL, pubkey, a_times_e, NULL);
         EC_POINT_add(curve, Ps, Ps, pubkey_a_e, nullptr);
+        //printECPointCoordinates(Ps);
         // 释放资源
         BN_free(r);
         BN_free(s);
@@ -268,10 +410,10 @@ bool schnorr_batch_verify(const std::vector<EC_POINT*>& pubkeys, const std::vect
 
     EC_POINT* LHS = EC_POINT_new(curve);
     EC_POINT_mul(curve, LHS, NULL, G, S, ctx);
-
+    //printECPointCoordinates(LHS);
     EC_POINT* RHS = EC_POINT_new(curve);
     EC_POINT_add(curve, RHS, Rs, Ps, ctx);
-
+    //printECPointCoordinates(RHS);
     bool result = (EC_POINT_cmp(curve, LHS, RHS, ctx) == 0);
 
 
@@ -286,38 +428,63 @@ bool schnorr_batch_verify(const std::vector<EC_POINT*>& pubkeys, const std::vect
     return result;
 }
 
+
 int main() {
-	EC_POINT* G = EC_POINT_new(curve);
-	const EC_POINT* generator = EC_GROUP_get0_generator(curve);
-	EC_POINT_copy(G, generator);
+    /*效率测试相关变量*/
+    LARGE_INTEGER beginTime;
+    LARGE_INTEGER endTime;
+    LARGE_INTEGER frequency;
+    double timeInMilliseconds = 0;
+    QueryPerformanceFrequency(&frequency);
 
-	BIGNUM* x = BN_new();
-	BIGNUM* y = BN_new();
-	EC_POINT_get_affine_coordinates(curve, G, x, y, NULL);
 
-	EC_POINT* pubkey = EC_POINT_new(curve);
-	EC_POINT_mul(curve, pubkey, x, NULL, NULL, NULL);
 
-    std::vector<unsigned char> msg(31, 0);
-
-    std::vector<unsigned char> signature(64);
-    signature = schnorr_sign(msg, x);
-
-    // 将 signature 添加到 std::vector 中
     std::vector<std::vector<unsigned char>> signature_vec;
-    signature_vec.push_back(signature);
-
-    // 将 pk 添加到 std::vector 中
     std::vector<EC_POINT*> pk_vec;
-    pk_vec.push_back(pubkey);
-
-    // 将 msg 添加到 std::vector 中
     std::vector<std::vector<unsigned char>> msg_vec;
-    msg_vec.push_back(msg);
-   
-    if (schnorr_batch_verify(pk_vec, msg_vec, signature_vec) == 1) {
-        std::cout << "验证通过";
+    for (int i = 0; i < 300; i++) {
+        EC_POINT* G = EC_POINT_new(curve);
+        const EC_POINT* generator = EC_GROUP_get0_generator(curve);
+        EC_POINT_copy(G, generator);
+
+        BIGNUM* k = BN_new();
+        BN_rand(k, 256, 0, 0);
+
+        BIGNUM* x = BN_new();
+        BN_hex2bn(&x, "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798");
+
+        EC_POINT* pubkey = EC_POINT_new(curve);
+        EC_POINT_mul(curve, pubkey, x, NULL, NULL, NULL);
+
+        std::vector<unsigned char> msg(31, 0);
+        std::vector<unsigned char> signature(64);
+        signature = schnorr_sign(msg, x);
+        QueryPerformanceCounter(&beginTime);
+        if (schnorr_verify(msg, pubkey, signature) == 0) {
+            std::cout << "单次验证通过\n";
+        }; 
+        QueryPerformanceCounter(&endTime);
+        timeInMilliseconds += ((endTime.QuadPart - beginTime.QuadPart) * 1000.0 / frequency.QuadPart);
+
+        // 将 signature 添加到 std::vector 中
+        signature_vec.push_back(signature);
+
+        // 将 pk 添加到 std::vector 中
+        pk_vec.push_back(pubkey);
+
+        // 将 msg 添加到 std::vector 中
+        msg_vec.push_back(msg);
     };
+    std::cout << "单次验证总用时: " << timeInMilliseconds << " ms" << std::endl;
+
+    QueryPerformanceCounter(&beginTime);
+    if (schnorr_batch_verify(pk_vec, msg_vec, signature_vec) == 1) {
+        std::cout << "批量验证通过\n";
+    };
+    QueryPerformanceCounter(&endTime);
+    timeInMilliseconds = ((endTime.QuadPart - beginTime.QuadPart) * 1000.0 / frequency.QuadPart);
+
+    std::cout << "批量验证用时: " << timeInMilliseconds << " ms" << std::endl;
 
 }
 	
